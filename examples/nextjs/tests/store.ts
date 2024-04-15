fe03b7e0156603b8e51763e42197453d42f5849a
AkaraChen <akarachen@outlook.com>
2024-04-15 14:20:07 +0800

    ci: setup

--- examples/nextjs/tests/store.ts
+++ examples/nextjs/tests/store.ts
@@ -1,9 +1,7 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
 const base = "http://localhost:7812";
 
 test("should work", async ({ page }) => {
 	await page.goto(base);
-	const sync = page.getByTestId("sync");
-	await expect(sync).toHaveText("Sync");
 });
